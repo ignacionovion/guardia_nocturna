e3de21a3b4666c0ca9cae79b72fbfed6ae260b5d
Ignacio Novion <ignacio.novion@tide.cl>
2026-03-17 00:35:33 -0300

    kasjd

--- resources/js/guardia-live/components/FirefighterCard.vue
+++ resources/js/guardia-live/components/FirefighterCard.vue
@@ -171,7 +171,7 @@ onUnmounted(() => document.removeEventListener('click', onDocClick, true));
                 :src="f.photo_url"
                 :alt="fullName"
                 class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
-                style="object-position: center 35%;"
+                style="object-position: center 50%;"
             />
             <div
                 v-else
