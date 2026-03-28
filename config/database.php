c43f07baa58fb5e73fa5f613f90d9ed24bacde45
Ignacio Novion <ignacio.novion@tide.cl>
2026-03-28 01:37:18 -0300

    lkdsjalkdjsaldkj

--- config/database.php
+++ config/database.php
@@ -32,7 +32,7 @@ return [
     'connections' => [
 
         'central' => [
-            'driver' => env('CENTRAL_DB_CONNECTION', env('DB_CONNECTION', 'mysql')),
+            'driver' => env('CENTRAL_DB_DRIVER', env('DB_CONNECTION', 'mysql')),
             'url' => env('CENTRAL_DB_URL', env('DB_URL')),
             'host' => env('CENTRAL_DB_HOST', env('DB_HOST', '127.0.0.1')),
             'port' => env('CENTRAL_DB_PORT', env('DB_PORT', '3306')),
@@ -46,9 +46,6 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'sqlite' => [
